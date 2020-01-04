c56f236eafc02bf5fef84949929d167c72a89618
unknown <kebcdy@gmail.com>
2020-01-04 14:44:15 +0800

    Updates

---
+++ precache-manifest.f8ec4471e869e4de8696197eae06bb04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50af1aa0e65950e07b2bd1b64a90e135",
+    "url": "/spa_blog/index.html"
+  },
+  {
+    "revision": "b5b8274099761d469d8a",
+    "url": "/spa_blog/static/css/main.c5c93fcb.chunk.css"
+  },
+  {
+    "revision": "33142bafcb79d831ace2",
+    "url": "/spa_blog/static/js/2.4ec55bf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spa_blog/static/js/2.4ec55bf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5b8274099761d469d8a",
+    "url": "/spa_blog/static/js/main.245959c2.chunk.js"
+  },
+  {
+    "revision": "90ac5f949eedec7a224f",
+    "url": "/spa_blog/static/js/runtime-main.60e3959a.js"
+  }
+]);
